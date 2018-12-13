807991099139e89ef6db377d3d0e4731d3b01824
Sarah Dixon <dixonspectives@gmail.com>
2018-12-13 13:10:45 -0600

    sorted columns alphabetically

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,10 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -41,6 +38,7 @@ public class JobData {
 
             if (!values.contains(aValue)) {
                 values.add(aValue);
+                Collections.sort(values);
             }
         }
 
@@ -79,6 +77,7 @@ public class JobData {
 
             if (aValue.contains(value)) {
                 jobs.add(row);
+
             }
         }
 
